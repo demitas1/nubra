5ab18ede8b3c5a7e050fed191b068fbac37adcc0
demitas <hi.demitas@gmail.com>
2018-01-13 20:55:08 +0900

    get sure dat file

--- util2ch.py
+++ util2ch.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 import codecs
@@ -22,9 +22,9 @@ data_dir = './dat'
 file_bbsmenu = 'ita_list.txt'
 
 
-def get_from_server(url):
-    # wait 1sec to avoid server overload
-    time.sleep(1)
+def get_from_server(url, charset=None):
+    # wait 2sec to avoid server overload
+    time.sleep(2)
 
     # get html from url
     try:
@@ -41,8 +41,11 @@ def get_from_server(url):
 
     # decode html content text
     data_bytes = response.read()
-    char_guessed = chardet.detect(data_bytes) 
-    content = data_bytes.decode(char_guessed['encoding']) 
+    if charset is None:
+        char_guessed = chardet.detect(data_bytes)
+        content = data_bytes.decode(char_guessed['encoding'])
+    else:
+        content = data_bytes.decode(charset)
     return content
 
 
@@ -126,3 +129,33 @@ if __name__ == '__main__':
     for i in range(0, min(10, len(sure_list))):
         dat_name, dat_title, dat_resu = sure_list[i]
         print("{}:{} ({})".format(dat_name, dat_title, dat_resu))
+
+    # generate dat url/local path for the top sure
+    dat_name, dat_title, dat_resu = sure_list[0]
+    dat_url = urljoin(ita_url, 'dat/' + dat_name)
+    print("dat url:[{}]".format(dat_url))
+    dat_local_path = os.path.join(dat_ita_root, dat_name)
+    print("dat local path:[{}]".format(dat_local_path))
+
+    # compare local dat file to check newer resu
+    if os.path.exists(dat_local_path):
+        with open(dat_local_path, 'r') as f:
+            dat_content = f.readlines()
+            n_resu_dat_local = len(dat_content)
+    else:
+        n_resu_dat_local = 0
+    n_resu_dat_server = int(dat_resu)
+    print("server[{}]:local[{}]".format(n_resu_dat_server, n_resu_dat_local))
+    exists_newer_dat = n_resu_dat_server > n_resu_dat_local
+
+    # get latest dat file from the server
+    if exists_newer_dat:
+        dat_content = get_from_server(dat_url)
+        # save dat file
+        with codecs.open(dat_local_path, 'w', 'utf-8') as f:
+            f.write(dat_content)
+
+    # show last 3 resu of the sure
+    dat_lines = dat_content.split('\n')
+    for l in dat_lines[-3:]:
+        print(l)
